75c5a33d57c979eb4ff7d9e2fdc7e61622a7806a
Abhay <kiyashkoandrey1988@gmail.com>
2023-06-07 20:14:41 +0300

    Reformat code

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,10 @@ public class Main {
     public static void main(String[] args) {
         int amount = 100;
         int bonus = 20;
-                int mile = amount / bonus;
+        int mile = amount / bonus;
 
-                System.out.println("Количество бонусных миль: " + mile);
+        System.out.println("Количество бонусных миль: " + mile);
     }
 }
+
+
